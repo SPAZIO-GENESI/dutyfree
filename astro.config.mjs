a8390f4e3c66400c84d7a1c2521f5837a8788bfd
maxAlone <mascam64@gmail.com>
2025-12-02 22:04:17 +0100

    sitemap auto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: 'static',
-  site: 'https://SPAZIO-GENESI.github.io/dutyfree',
+  
   outDir: 'docs',        // <-- qui, non dentro build
-  publicDir: 'public'    // <-- qui, non dentro build
+  publicDir: 'public',    // <-- qui, non dentro build
+
+  site: "https://dutyfree.spaziogenesi.org", // URL pubblico del sito
+  integrations: [sitemap()],
 });
